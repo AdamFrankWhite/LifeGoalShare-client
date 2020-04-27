875e8252b2eccdadded41438495b2720c96578ac
Adam White <adamfrankwhite@gmail.com>
2020-04-27 13:07:53 +0100

    refactored to stateless comp

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,59 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReplyInput from "./ReplyInput";
 import moment from "moment";
 import { connect } from "react-redux";
 
-class Comment extends Component {
-  constructor() {
-    super();
-    this.showReplyInput = this.showReplyInput.bind(this);
-    this.state = {
-      showComments: false,
-      showReplyInput: false,
-    };
-  }
+function Comment(props) {
+  // const [toggleComments, setToggleComments] = useState(false);
+  const [toggleReplyInput, setToggleReplyInput] = useState(false);
 
-  showReplyInput() {
-    this.setState({ showReplyInput: !this.state.showReplyInput });
-    console.log(window.localStorage.getItem("access_token"));
-  }
-  render() {
-    {
-      /* Check if comment deleted */
-    }
-    let author =
-      this.props.comment.comment !== "DELETED"
-        ? this.props.comment.author + ": "
-        : "";
+  // Check if comment deleted
 
-    let comment = this.props.comment.comment;
+  let author =
+    props.comment.comment !== "DELETED" ? props.comment.author + ": " : "";
+  let comment = props.comment.comment;
 
-    return (
-      <div className="goal-comment-cont" style={this.props.margin}>
-        <div className="goal-comment">
-          <p>
-            {author} {comment}
-          </p>
-          {this.props.loggedIn && (
-            <span
-              onClick={() => this.showReplyInput()}
-              className="comment-reply-btn"
-            >
-              {this.state.showReplyInput ? "Cancel" : "Reply"}
-            </span>
-          )}
-        </div>
-        <p>{moment(this.props.comment.createdAt).fromNow()}</p>
-        {this.state.showReplyInput && (
-          <ReplyInput
-            lifeGoalID={this.props.lifeGoalID}
-            parents={this.props.parents}
-          />
+  return (
+    <div className="goal-comment-cont" style={props.margin}>
+      <div className="goal-comment">
+        <p>
+          {author} {comment}
+        </p>
+        {props.loggedIn && (
+          <span
+            onClick={() => setToggleReplyInput(!toggleReplyInput)}
+            className="comment-reply-btn"
+          >
+            {toggleReplyInput ? "Cancel" : "Reply"}
+          </span>
         )}
       </div>
-    );
-  }
+      <p>{moment(props.comment.createdAt).fromNow()}</p>
+      {toggleReplyInput && (
+        <ReplyInput lifeGoalID={props.lifeGoalID} parents={props.parents} />
+      )}
+    </div>
+  );
 }
+
 const mapStateToProps = function (state) {
   return {
     // profile: state.user.profile,
