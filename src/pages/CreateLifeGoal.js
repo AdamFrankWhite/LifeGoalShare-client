38066e7a65aac50dab30a090fed872b74ba83405
Adam White <adamfrankwhite@gmail.com>
2020-05-01 12:46:27 +0100

    multistepform start

--- src/pages/CreateLifeGoal.js
+++ src/pages/CreateLifeGoal.js
@@ -5,10 +5,11 @@ import { postNewLifeGoal } from "../redux/actions/lifegoalActions";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 
 function CreateLifeGoal(props) {
-  const [lifeGoalName, setLifeGoalTitle] = useState("");
+  const [lifeGoalName, setLifeGoalName] = useState("");
   const [lifeGoalDescription, setLifeGoalDescription] = useState("");
   const [initialPostName, setInitialPostName] = useState("");
   const [initialPostContent, setInitialPostContent] = useState("");
+  const [stepCount, setStepCount] = useState(0);
   const initialPostHeaderImage = "some image";
 
   const newLifeGoalData = {
@@ -18,37 +19,59 @@ function CreateLifeGoal(props) {
     initialPostContent,
     initialPostHeaderImage,
   };
+
   return (
     <div className="create-lifegoal-cont">
       <h1>Create New LifeGoal</h1>
-      <input
-        placeholder="Your LifeGoal title..."
-        onChange={(e) => {
-          setLifeGoalTitle(e.target.value);
-        }}
-      />
-      <textarea
-        placeholder="LifeGoal description"
-        onChange={(e) => setLifeGoalDescription(e.target.value)}
-      ></textarea>
+      {/* Step 1 */}
+      {stepCount === 0 && (
+        <div>
+          <input
+            placeholder="Your LifeGoal title..."
+            value={lifeGoalName}
+            onChange={(e) => setLifeGoalName(e.target.value)}
+          />
+          <textarea
+            value={lifeGoalDescription}
+            placeholder="LifeGoal description"
+            onChange={(e) => setLifeGoalDescription(e.target.value)}
+          ></textarea>
+          <button onClick={() => setStepCount(stepCount + 1)}>Next</button>
+        </div>
+      )}
+      {/* Step 2 */}
+      {stepCount === 1 && (
+        <div>
+          <input
+            placeholder="Your first post title..."
+            value={initialPostName}
+            onChange={(e) => {
+              setInitialPostName(e.target.value);
+            }}
+          />
+          <textarea
+            placeholder="Your first post content"
+            value={initialPostContent}
+            onChange={(e) => setInitialPostContent(e.target.value)}
+          ></textarea>
+          <button onClick={() => setStepCount(stepCount - 1)}>Back</button>
+          <button onClick={() => setStepCount(stepCount + 1)}>Next</button>
+        </div>
+      )}
+      {/* Step 3 */}
+      {stepCount === 2 && (
+        <div>
+          <button onClick={() => setStepCount(stepCount - 1)}>Back</button>
+          <span
+            className="create-lifegoal-btn"
+            onClick={() => props.postNewLifeGoal(newLifeGoalData)}
+          >
+            Create LifeGoal
+            <AddCircleOutlineIcon />
+          </span>
+        </div>
+      )}
       <br></br>
-      <input
-        placeholder="Your first post title..."
-        onChange={(e) => {
-          setInitialPostName(e.target.value);
-        }}
-      />
-      <textarea
-        placeholder="Your first post content"
-        onChange={(e) => setInitialPostContent(e.target.value)}
-      ></textarea>
-      <span
-        className="create-lifegoal-btn"
-        onClick={() => props.postNewLifeGoal(newLifeGoalData)}
-      >
-        Create LifeGoal
-        <AddCircleOutlineIcon />
-      </span>
     </div>
   );
 }
