70f86f8ee60c639ffe353964c1eed716fd780ec8
Adam White <adamfrankwhite@gmail.com>
2020-05-13 10:09:14 +0100

    made fc, cleaned up

--- src/App.js
+++ src/App.js
@@ -27,109 +27,68 @@ import LifeGoalMain from "./components/LifeGoalMain";
 //History
 import history from "./history";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.handleRedirect = this.handleRedirect.bind(this);
-    this.getComments = this.getComments.bind(this);
-    this.state = {
-      username: "",
-      password: "",
-      userData: null,
-      sessionToken: "",
-      redirect: "",
-      loggedIn: false,
-      isAuthenticated: false,
-      userComments: [],
-      lifeGoals: [],
-    };
-  }
+//Socketio
 
-  getComments() {
-    axios
-      .get("http://localhost:5000/users/comments", {
-        headers: { Authorization: window.localStorage.getItem("access_token") },
-      })
-      .then((data) => {
-        this.setState({ userComments: data.data });
-      });
-  }
+import socketIOClient from "socket.io-client";
 
-  componentDidMount() {
-    // this.setState({ isAuthenticated: store.getState().user.authenticated });
-    //needs to also authenticate on update, otherwise out of sync
-  }
-
-  handleRedirect(page) {
-    let path = page ? page : "";
-    return <Redirect to={`/${path}`} />;
-  }
-
-  render() {
-    const lifeGoalRoutes = this.props.lifegoals.map((lifeGoal) => {
-      return (
-        <Route
-          path={`/lifegoals/${lifeGoal._id}`}
-          render={(props) => (
-            <LifeGoalMain {...props} key={lifeGoal._id} data={lifeGoal} />
-          )}
-        />
-      );
-    });
-
-    const userRoutes = this.props.user.users.map((user) => {
-      return (
-        <Route
-          path={`/user/${user._id}`}
-          render={(props) => <User {...props} data={user} />}
-        />
-      );
-    });
-
+function App(props) {
+  const socketio = socketIOClient();
+  const lifeGoalRoutes = props.lifegoals.map((lifeGoal) => {
     return (
-      <div className="App">
-        <Router>
-          {this.handleRedirect()}
-          <NavBar logout={this.handleLogout} />
-
-          <Switch>
-            {lifeGoalRoutes}
-            {userRoutes}
-            <Route
-              path="/lifegoal/add"
-              render={(props) => <CreateLifeGoal {...props} />}
-            />
-            <Route exact path="/" render={(props) => <Home {...props} />} />
-            {/* Protected routes */}
-            <Route path="/logout">
-              <Redirect to="/"></Redirect>
-            </Route>
-            <Route
-              path="/dashboard"
-              render={(props) => (
-                <Dashboard
-                  {...props}
-                  getComments={this.getComments}
-                  myComments={this.state.userComments}
-                />
-              )}
-            />
-
-            <Route path="/post" exact render={(props) => <Post {...props} />} />
-
-            <Route path="/login">
-              {this.props.user.authenticated ? (
-                <Redirect to="/dashboard" />
-              ) : (
-                <LogIn />
-              )}
-            </Route>
-            <Route path="/signup" component={SignUp} />
-          </Switch>
-        </Router>
-      </div>
+      <Route
+        path={`/lifegoals/${lifeGoal._id}`}
+        render={(props) => (
+          <LifeGoalMain {...props} key={lifeGoal._id} data={lifeGoal} />
+        )}
+      />
     );
-  }
+  });
+
+  const userRoutes = props.user.users.map((user) => {
+    return (
+      <Route
+        path={`/user/${user._id}`}
+        render={(props) => <User {...props} data={user} />}
+      />
+    );
+  });
+
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+
+        <Switch>
+          {lifeGoalRoutes}
+          {userRoutes}
+          <Route
+            path="/lifegoal/add"
+            render={(props) => <CreateLifeGoal {...props} />}
+          />
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          {/* Protected routes */}
+          <Route path="/logout">
+            <Redirect to="/"></Redirect>
+          </Route>
+          <Route
+            path="/dashboard"
+            render={(props) => <Dashboard {...props} />}
+          />
+
+          <Route path="/post" exact render={(props) => <Post {...props} />} />
+
+          <Route path="/login">
+            {props.user.authenticated ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <LogIn />
+            )}
+          </Route>
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
